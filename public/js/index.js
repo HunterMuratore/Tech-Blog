d75bc64fd0e34249f7f9ea955c5a015536428233
HunterMuratore <muratoreh@gmail.com>
2023-10-22 13:38:50 -0400

    Finished update post route allowing user to update their posts on the dashboard

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function () {
     const inputField = $('#inputField');
     const charCount = $('#charCount');
     const form = $('#postForm');
     const submitButton = $('#submitBtn');
     const updateBtn = $("#updateBtn");
 
-    inputField.on('input', function() {
+    inputField.on('input', function () {
         const inputValue = inputField.val();
         const inputLength = inputValue.length;
-        
+
         charCount.text(inputLength);
 
         // Disable the submit button if character count exceeds 500
@@ -20,7 +20,7 @@ $(document).ready(function() {
     });
 
     // Show/hide the character count error popup
-    form.on('submit', function(event) {
+    form.on('submit', function (event) {
         const inputLength = inputField.val().length;
         if (inputLength > 500) {
             event.preventDefault(); // Prevent form submission
@@ -31,29 +31,45 @@ $(document).ready(function() {
         var postContainer = $(this).closest(".post");
         var postText = postContainer.find(".post-text");
         var editInput = postContainer.find(".edit-post-input");
-
+    
         // Show the input field, hide the text
         postText.hide();
         editInput.show();
-
+    
         // Set the input field's value to the current text
         editInput.val(postText.text());
 
-        // When the user presses enter (13), save the edited text
-        editInput.keypress(function (e) {
-            if (e.which === 13) {
-                var newText = editInput.val();
-                postText.text(newText);
+        // Fetch the postId associated with the post text
+        fetch(`/getPostId?text=${encodeURIComponent(postText.text())}`, {
+            method: 'GET',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error fetching postId');
+                }
+                return response.json();
+            })
+            .then(data => {
+                var postId = data.postId;
 
-                // Send the updated text to the server
-                var postId = postContainer.data("post-id");
-                updatePostText(postId, newText);
+                // When the user presses enter (13), save the edited text
+                editInput.keypress(function (e) {
+                    if (e.which === 13) {
+                        var newText = editInput.val();
+                        postText.text(newText);
 
-                // Hide the input and show the text again
-                editInput.hide();
-                postText.show();
-            }
-        });
+                        // Send the updated text and postId to the server
+                        updatePostText(postId, newText);
+
+                        // Hide the input and show the text again
+                        editInput.hide();
+                        postText.show();
+                    }
+                });
+            })
+            .catch(error => {
+                console.log(error.message);
+            });
     });
 
     function updatePostText(postId, newText) {
@@ -62,12 +78,12 @@ $(document).ready(function() {
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ postId, newText }),
+            body: JSON.stringify({ postId, newText })
         })
-        .then(response => {
-            if (!response.ok) {
-                console.log('Error updating the post text');
-            }
-        });
+            .then(response => {
+                if (!response.ok) {
+                    console.log('Error updating the post text');
+                }
+            });
     }
 });
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const app = express();
 /* Handlebars Middleware */
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 /* Load Session Middleware */
